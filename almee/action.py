5444002cf1ac2e684f5a6838afc81c192cded53b
almee <byuy@qq.com>
2016-07-26 13:54:49 +0800

    get applicationID by name

--- almee/action.py
+++ almee/action.py
@@ -3,6 +3,7 @@ import os
 import re
 import time
 import subprocess
+from random import choice
 
 
 class action:
@@ -10,60 +11,62 @@ class action:
     def __init__(self):
         """Constructor"""
 
-    def killApplicationByID(self, applicationID):
-        order = 'bash /usr/local/hadoop-2.7.2/bin/yarn application -kill ' + applicationID
+    def killApplicationById(self, applicationId):
+        order = 'bash /usr/local/hadoop-2.7.2/bin/yarn application -kill ' + applicationId
         #feedback = os.popen(order).read()
-        feedback = subprocess.Popen(order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        feedback = subprocess.Popen(
+            order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         feedback = feedback.stdout.readline()
         return feedback
 
-    def getApplicationIDSet(self, arg='RUNNING'):
+    def getApplicationName2IdSet(self, arg='RUNNING'):
         order = 'bash /usr/local/hadoop-2.7.2/bin/yarn application -list -appStates ' + arg
         feedback = os.popen(order).read()
-        regix = re.compile(r'application_\d+_\d+')
-        IDset = regix.findall(feedback)
-        IDset.sort()
-        return IDset
+        regix = re.compile(r'application_\d+_\d+\s+\d+')
+        IdAndName = regix.findall(feedback)
+        IdAndName.sort()
+        Idset = {}
+        for item in IdAndName:
+            tmp = item.split('\t')
+            Id = tmp[0].replace(' ', '')
+            Name = tmp[1].replace(' ', '')
+            Idset[Name] = Id
+        return Idset
 
     def submitJob(self, className, driverMemory, executorMemory, executorCores, jarPath, args=""):
+        name = ''.join([choice('0123456789') for i in range(24)])
         order = 'bash /usr/local/spark-1.6.2-bin-without-hadoop/bin/spark-submit --class ' + className + ' --master yarn --deploy-mode cluster --driver-memory ' + \
-            driverMemory + ' --executor-memory ' + executorMemory + \
-                ' --executor-cores ' + executorCores + ' --queue default ' \
+            driverMemory + ' --executor-memory ' + executorMemory + ' --name ' + name + \
+                ' --executor-cores ' + executorCores + ' --queue default '\
                 + jarPath + ' ' + args
-        applicationIDSet = self.getApplicationIDSet('ALL')
-        applicationNum = len(applicationIDSet)
         # feedback = os.popen(order).read()
-        feedback = subprocess.Popen(order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-        applicationIDSet = self.getApplicationIDSet('ALL')
-        applicationNum2 = len(applicationIDSet)
-        if applicationNum == applicationNum2:
-        	result = feedback.stdout.readline()
-        else:
-	        if len(applicationIDSet) == 0:
-	            time.sleep(2)
-	        result = applicationIDSet[-1]
-        # print '-------------------------------------------------------------------------------'
-        # print result
-        # print
-        # '-------------------------------------------------------------------------------'
-        return result
+        feedback = subprocess.Popen(
+            order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        while 1:
+            applicationIdSet = self.getApplicationName2IdSet('ALL')
+            if applicationIdSet.has_key(name):
+                applicationId = applicationIdSet[name]
+                break
+            else:
+                time.sleep(1)
+        return applicationId
 
-    def getStatus(self, applicationID):
-        order = 'bash /usr/local/hadoop-2.7.2/bin/yarn application -status ' + applicationID
+    def getStatus(self, applicationId):
+        order = 'bash /usr/local/hadoop-2.7.2/bin/yarn application -status ' + applicationId
         feedback = os.popen(order).read()
         feedback = feedback.replace('\n', '')
         first = feedback.split('\t')
         report = {}
-        #for item in first:
+        # for item in first:
         for index in range(0, 16):
             item = first[index]
             second = item.split(' : ')
             report[second[0]] = second[1]
-        report['Application Report'] = applicationID
+        report['Application Report'] = applicationId
         return report
 
-    def getLog(self, applicationID):
-        order = 'bash /usr/local/hadoop-2.7.2/bin/yarn logs -applicationId  ' + applicationID
+    def getLog(self, applicationId):
+        order = 'bash /usr/local/hadoop-2.7.2/bin/yarn logs -applicationId  ' + applicationId
         feedback = os.popen(order).read()
         # bad response
         containers = feedback.split('Container: ')
@@ -82,14 +85,16 @@ class action:
         # order = 'hadoop fs -put ' + filePath + ' ' + filePathHDFS
         order = 'bash /usr/local/bash/hdfs-put.sh ' + filePath + ' ' + filePathHDFS
         #feedback = os.popen(order).read()
-        feedback = subprocess.Popen(order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        feedback = subprocess.Popen(
+            order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         feedback = feedback.stdout.readline()
         return feedback
 
     def makeDirectory(self, filePathHDFS):
         order = 'bash /usr/local/bash/hdfs-mkdir.sh ' + filePathHDFS
         #feedback = os.popen(order).read()
-        feedback = subprocess.Popen(order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        feedback = subprocess.Popen(
+            order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         feedback = feedback.stdout.readline()
         return feedback
 
@@ -97,7 +102,8 @@ class action:
         # order = 'hadoop fs -get ' + filePathHDFS + ' ' + filePath
         order = 'bash /usr/local/bash/hdfs-get.sh ' + filePathHDFS + ' ' + filePath
         #feedback = os.popen(order).read()
-        feedback = subprocess.Popen(order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        feedback = subprocess.Popen(
+            order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         feedback = feedback.stdout.readline()
         return feedback
 
@@ -107,10 +113,14 @@ class action:
         else:
             order = 'bash /usr/local/bash/hdfs-rmdir.sh ' + filePathHDFS
         #feedback = os.popen(order).read()
-        feedback = subprocess.Popen(order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+        feedback = subprocess.Popen(
+            order, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         feedback = feedback.stdout.readline()
         return feedback
 
-#if __name__ == '__main__':
-#        ac = action()
-#        ac.submitJob('org.apache.spark.examples.SparkPi', '4g', '4g', '7', 'hdfs:///spark-examples-1.6.2-hadoop2.2.0.jar', '1000')
+# if __name__ == '__main__':
+#       ac = action()
+#       ID = ac.submitJob('org.apache.spark.examples.SparkPi', '4g', '4g', '7', 'hdfs:///spark-examples-1.6.2-hadoop2.2.0.jar', '1000')
+#       print '-------------------------------------------------'
+#       print ID
+#       print '-------------------------------------------------'
